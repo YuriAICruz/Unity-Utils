2901ae4c36eaa1ac4f5da928f0705eb669a334a3
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-09-26 15:11:29 -0300

    add generic script for auto deleting on start

---
+++ Assets/Scripts/AutoDelete.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Graphene.Utils
+{
+    public class AutoDelete : MonoBehaviour
+    {
+        public float Timeout;
+
+        void Start()
+        {
+            Invoke(nameof(DestroySelf), Timeout);
+        }
+
+        void DestroySelf()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
