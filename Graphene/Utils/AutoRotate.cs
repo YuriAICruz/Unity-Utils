de4eb1076f4fb604a9af8614e8c3f2ffcccae0b9
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-10-10 20:14:25 -0300

    add auto rotate

---
+++ Graphene/Utils/AutoRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graphene.Utils
+{
+	public class AutoRotate : MonoBehaviour
+	{
+		public float Speed;
+		
+		void Update()
+		{
+			transform.Rotate(Vector3.up, Time.deltaTime*Speed, Space.World);
+		}
+	}
+}
