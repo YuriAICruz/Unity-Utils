b51974ec5d7852270d29d050d31d0e6873f1eb24
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-11-08 20:39:21 -0200

    add Vector extensiion for get intersection

--- Graphene/Utils/ListShuffle.cs
+++ Graphene/Utils/ListShuffle.cs
@@ -19,4 +19,29 @@ namespace Graphene.Utils
             return list;
         }
     }
+
+    public static class Vector3Extensions
+    {
+        public static bool Intersection(this Vector3 linePoint1, Vector3 a, Vector3 linePoint2, Vector3 b, out Vector3 intersection)
+        {
+            var c = linePoint2 - linePoint1;
+            var crossVec1and2 = Vector3.Cross(a, b);
+            var crossVec3and2 = Vector3.Cross(c, b);
+ 
+            var planarFactor = Vector3.Dot(c, crossVec1and2);
+            
+            //is coplanar, and not parrallel
+            if (Mathf.Abs(planarFactor) > 0.0001f && crossVec1and2.sqrMagnitude > 0.0001f)
+            {
+                float s = Vector3.Dot(crossVec3and2, crossVec1and2) / crossVec1and2.sqrMagnitude;
+                intersection = linePoint1 + (a * s);
+                return true;
+            }
+            else
+            {
+                intersection = Vector3.zero;
+                return false;
+            }
+        } 
+    }
 }
