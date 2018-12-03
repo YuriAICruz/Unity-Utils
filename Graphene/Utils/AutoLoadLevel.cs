fd1ffb9c3c55876b5769f2ef0f479e0e124f714f
Yuri Cruz <yuriaicruz@gmail.com>
2018-12-03 03:42:53 -0200

    add autoloadlevel, deepcopy

---
+++ Graphene/Utils/AutoLoadLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Graphene.Utils
+{
+    public class AutoLoadLevel : MonoBehaviour
+    {
+        public float Delay;
+        public int Index;
+
+        private void Awake()
+        {
+            Invoke(nameof(LoadScene), Delay);
+        }
+
+        void LoadScene()
+        {
+            SceneManager.LoadSceneAsync(Index);
+        }
+    }
+}
---
+++ Graphene/Utils/DeepCopy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Graphene.Utils
+{
+    public static class DeepCopyUtiils
+    {
+        public static T DeepCopy<T>(this T obj)
+
+        {
+            if (!typeof(T).IsSerializable)
+
+            {
+                throw new Exception("The source object must be serializable");
+            }
+
+            if (Object.ReferenceEquals(obj, null))
+
+            {
+                throw new Exception("The source object must not be null");
+            }
+
+            T result = default(T);
+
+            using (var memoryStream = new MemoryStream())
+
+            {
+                var formatter = new BinaryFormatter();
+
+                formatter.Serialize(memoryStream, obj);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                result = (T) formatter.Deserialize(memoryStream);
+
+                memoryStream.Close();
+            }
+
+            return result;
+        }
+    }
+}
